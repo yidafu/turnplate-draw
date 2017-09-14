627a91efc4d2c70583629276f91bf36b1589ee3f
yidafu <1427019390@qq.com>
2017-09-14 12:18:19 +0800

    this branch complete the first edtion.

--- js/main.js
+++ js/main.js
@@ -14,17 +14,25 @@
  */
 (function( window, undefined ){
     turnplate = function( id, data, config ) {
+
         return new turnplate.fn.init( id, data, config );
+
     }
     turnplate.fn = turnplate.prototype = {
 
-        constructor: turnplate,
+
 
         /**
         * 这是你的幸运与黄金
         * @type {Number}
         */
         luckyAndCold: 0,
+
+        /**
+         * 中奖数据
+         * @type {Number}
+         */
+         winning : 0,
 
         itself : this,
 
@@ -34,55 +42,71 @@
          */
         turnplate : document.createElement('div'),
 
+        /**
+         * 旋转角度
+         * @type {Number}
+         */
         degree : 0,
 
+        /**
+         * 抽奖数据
+         * @type {Array}
+         */
+        data : [],
+
+        /**
+         * 背景颜色配置数据
+         * @type {Array}
+         */
+        bgColors : ['#eee', '#233'],
         /**
          * 初始化函数
          * @param  {String} [id='turnplateContainer'] 转盘容器的 id
          * @param  {Array}  [data=[]]                 数据转盘显示数据，格式如下：
          *                                                  [{name    : String,
-         *                                                    img     : String,
-         *                                                    bgColor : String,
-         *                                                    surplus : Number},
+         *                                                    num : Number
+         *                                                    probability: Number},
          *                                                    ... ... ]
          * @return {Object}                           返回转盘对象本身
          */
-        init: function (id = 'turnplateContainer', data = [] ) {
+         /**
+          * 构造函数
+          * @type {Function}
+          */
+        constructor: turnplate,
 
-
-
+        /**
+         * [description]
+         * @param  {String} [id='turnplateContainer'] 转盘容器ID
+         * @param  {Array}  [data=[]]                 抽奖数据
+         * @return {void}
+         */
+        init: function ( id = 'turnplateContainer', data = [] ) {
             /**
-             * 最外层大容器
+             * 最外层容器
              * @type {Object}
              */
-            this.container = document.getElementById(id);
-            // /**
-            //  * 转盘容器
-            //  * @type {object}
-            //  */
-            // this.turnplate    = document.createElement('div');
+            turnplate.container = this.container = document.getElementById(id);
             this.turnplate.id = 'turnplate';
 
             /**
              * 转盘半径
              * @type {Number}
              */
-            this.turnplateRadius = 200;
+            turnplate.turnplateRadius = this.turnplateRadius = 200;
 
             /**
              * 转盘的块数
              * @type {Number}
              */
-            this.count       = 20;
-
-            this.names       = ['一等奖','二等奖','二等奖','二等奖','二等奖','二等奖','二等奖','二等奖','二等奖','二等奖','二等奖'];
-            // this.imgs        = [];
-            // this.bgColors     = [];
-            // this.surpluses   = [];
-
+            turnplate.count = this.count = 12;
+            turnplate.data = this.data = data;
             return this;
         },
-
+        /**
+         * 抽奖转盘的绘制函数
+         * @return {void}
+         */
         drawTurnplate: function() {
 
             /**
@@ -91,8 +115,7 @@
              * @type {float}
              */
             var itemAngle = (Math.PI / this.count) * 2;
-            // console.log('in line 33:');
-            // console.log(itemAngle);
+
             /**
              * 转盘三角形块的的高
              * @type {Number}
@@ -112,15 +135,10 @@
             initItem.id = 'initItem';
             initItem.className = 'initItem';
 
-            var img = document.createElement('img');
-            // img.src = 'http://tse2.mm.bing.net/th?id=OIP.nq3nPfEjg3Ro-qzBK8qP1AEsEr&pid=15.1';
-            img.src = null;
             var p = document.createElement('p');
             p.innerHTML = '1';
 
-            initItem.appendChild(img);
             initItem.appendChild(p);
-            // console.log(initItem);
 
             // @TODO 这里得处理一下 类似于奇数份的问题
             // 自动生成的话，这问题应该由客户端程序员解决
@@ -133,14 +151,16 @@
 
             // 制造其他三角形子块，并旋转 itemAngle 度
             for ( var i = 0 ; i < this.count ; i ++ ) {
-                // console.log(itemAngle);
+
                 var div = initItem.cloneNode(true);
 
                 // @TODO 在这里填充数据
-                if( i % 2 == 0 ) {
-                    div.childNodes[1].innerHTML = this.names[i / 2];
+                if ( i == 0 ) {
+                    div.firstChild.innerHTML = this.data[0].name + ( 12 - i);
+                } else if( i % 2 == 0 ) {
+                    div.firstChild.innerHTML = this.data[1].name + (12 - i);
                 } else {
-                    div.childNodes[1].innerHTML = '三等奖';
+                    div.firstChild.innerHTML = this.data[2].name + (12 - i);
                 }
                 // div.childNodes[0].src = this.imgs[i];
 
@@ -148,13 +168,16 @@
                 div.style.borderLeftWidth = halfItemWidth;
                 div.style.borderRightWidth = halfItemWidth;
                 div.style.transform = 'rotate(' + itemAngle * 57.3 * i +'deg)';
-                div.style.borderTopColor = (i + 1) % 2 == 0 ? '#888' : '#222';
-                // div.style.borderTopColor = this.bgColors[i];
+                div.style.borderTopColor = (i + 1) % 2 == 0 ? this.bgColors[0] : this.bgColors[1];
                 this.turnplate.appendChild(div);
             }
             this.container.appendChild(this.turnplate);
         },
 
+        /**
+         * 抽奖指针的绘制函数
+         * @return {void}
+         */
         drawTrianglePointer : function() {
             var pointerBox    = document.createElement('div');
             pointerBox.id = 'pointerBox';
@@ -164,9 +187,11 @@
 
             var pointerCircle = document.createElement('div');
             pointerCircle.id = 'pointerCircle';
-            // console.log(self);
+
             pointerCircle.onclick = function(itself) {
+                // @FIXME 这里的节点寻找太麻烦了
                 itself.view.turnplate.fn.fisrtRotationForce();
+
             }
 
             pointerBox.appendChild(trianglePointer);
@@ -175,30 +200,47 @@
             this.container.appendChild(pointerBox);
         },
         /**
-         *
-         * @param  {Number} [range=100] [description]
-         * @return {[type]}             [description]
+         *  抽奖的主要函数，这个方法决定了抽奖的概率
+         * @param  {Number} [range=60]  总概率
+         * @return {Number}             一个合格抽奖结果
          */
-        godGiveYouLuckyAndGold : function (range = 100) {
+        godGiveYouLuckyAndGold : function (range = 60) {
             /**
-            * 生成的 1 到 100 的随机数
+            * 生成的 1 到 60 的随机数
             * @type {Number}
             */
+
             var randomNum = parseFloat(Math.random() * range) ;
-
-
-            //  @TODO 下面抽奖策略，目前比较简陋，待优化
-            if(randomNum < 5 && randomNum >= 0) {
-             this.luckyAndGold = 1
-            } else if( ( randomNum >= 20 && randomNum < 25 ) ||
-                     ( randomNum >= 40 && randomNum < 45 ) ||
-                     ( randomNum >= 60 && randomNum < 65 ) ||
-                     ( randomNum >= 80 && randomNum < 85 ) ) {
-             this.luckyAndGold = parseInt(randomNum / 20) * 4 + 1 ;
-            }else {
-             this.luckyAndGold = Math.ceil(randomNum / 5);
+            // @FIXME 这里的存在作用域问题，无法访问到 this.data
+            var data = window.turnplate.data;
+            var remainder = randomNum % 10;
+            if( parseInt( data[0].num ) == 0
+                &&  parseInt( data[1].num ) == 0
+                &&  parseInt( data[2].num ) == 0 ) {
+                    alert('奖品已经抽完了！');
             }
-
+            if(randomNum < 5 && randomNum >= 0) {
+                if ( data[0].num <= 0 ) {
+                    this.luckyAndGold = this.godGiveYouLuckyAndGold()
+                } else {
+                    this.luckyAndGold = 0
+                }
+                this.winning = 1;
+            } else if( remainder < 5 && remainder >= 0 ) {
+                if ( data[1].num <= 0 ) {
+                    this.luckyAndGold = this.godGiveYouLuckyAndGold()
+                } else {
+                    this.luckyAndGold = parseInt(randomNum / 5 );
+                }
+                this.winning = 2;
+            }else {
+                if ( data[2].num <= 0 ) {
+                    this.luckyAndGold = this.godGiveYouLuckyAndGold()
+                } else {
+                    this.luckyAndGold = parseInt(randomNum / 5);
+                }
+                this.winning = 3;
+            }
             return this.luckyAndGold;
         },
 
@@ -208,17 +250,17 @@
           */
          fisrtRotationForce : function() {
              var luckyAndGold = this.godGiveYouLuckyAndGold();
-            //  luckyAndGold = 0;
-             this.degree = 3600 + ( ( 360 / 20 ) * luckyAndGold );
-             function theGraceOfGod(grace){
-                 alert(grace);
+             var winning = this.winning;
+             if (luckyAndGold == null ) {
+                 return;
              }
-            // (function(luckyAndGold){console.log('done');alert(luckyAndGold);})();
-            //  setTimeout( theGraceOfGod(luckyAndGold) , 6000 );
-            //  setTimeout( "function(luckyAndGold){console.log('done');alert(luckyAndGold);}", 6000 );
-            //
-            // 参见：<a herf="http://www.cnblogs.com/leon2016/p/5932061.html">链接<a>
-            // 另参见：<a herf="http://www.jb51.net/article/40524.htm">链接<a>
+             this.degree = 3600 + ( ( 360 / 12 ) * luckyAndGold );
+             
+             this.turnplate.previousSibling.childNodes[1].setAttribute( 'onclick', true );
+             /**
+              * 参见：<a herf="http://www.cnblogs.com/leon2016/p/5932061.html">链接<a>
+              * 另参见：<a herf="http://www.jb51.net/article/40524.htm">链接<a>
+              */
             window.setTimeout(function(){
                 if( luckyAndGold == 0 ){
                     alert('一等奖');
@@ -227,15 +269,20 @@
                 } else {
                     alert('三等奖');
                 }
-                // @TODO 下面的代码把转盘重置会初始位置
-                this.turnplate.fn.degree = 0;
-                this.turnplate.fn.turnplate.style.transform = 'rotate(' + this.turnplate.fn.degree + 'deg)';
+                // @TODO 这里发送 ajax 请求
+                console.log('这里是发给后端的数据：||| ' + winning + ' |||');
             }, 6000)
-            //  setTimeout( theGraceOfGod, 6000 , luckyAndGold );
              this.turnplate.style.transform = 'rotate(' + this.degree + 'deg)';
-             console.log(this.degree);
+         },
+
+         getWinningNum : function() {
+            return this.winning;
+        },
+         draw : function () {
+             tp.drawTrianglePointer();
+             tp.drawTurnplate();
          }
-    },
+    };
 
     turnplate.fn.init.prototype = turnplate.fn;
 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
 
-var data = {"first_prize":"5","second_prize":"25","third_prize":"30"};
+/*
+    @TODO 只剩 ajax 没写
+*/
+
+
+var data = [{"name":"一等奖",
+            "num":"5",
+            "probability":"5"},
+            {"name":"二等奖",
+            "num":"45",
+            "probability":"25"},
+            {"name":"三等奖",
+            "num":"150",
+            "probability":"30"}]
 
 var tp = turnplate('container', data );
-tp.drawTrianglePointer();
-tp.drawTurnplate();
-console.log('last');
-console.log(tp.lackAndGold);
+tp.draw()
